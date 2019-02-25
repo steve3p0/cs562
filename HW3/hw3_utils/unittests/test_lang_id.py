0f9a0cb57d746997b815125edf60eaac7dc28d58
steve3p0 <sbraich@bpdx.edu>
2019-02-24 23:05:59 -0800

    test_train_model_multi

--- HW3/hw3_utils/unittests/test_lang_id.py
+++ HW3/hw3_utils/unittests/test_lang_id.py
@@ -72,10 +72,14 @@ class TestLangID(unittest.TestCase):
     #   RuntimeError: Trying to backward through the graph a second time,
     #   but the buffers have already been freed.
     #   Specify retain_graph=True when calling backward the first time.
+    #
     # This error is NOT reproduced here in the unittest.
+    #
     # This error in not guaranteed to be raised, as per discussion here.
     #   https://discuss.pytorch.org/t/understanding-graphs-and-state/224/2?u=kharshit
     # I tried implementing some of the workarounds to no avail, but the same code works here
+    #
+    # Futhermore, the multinomial test code in the Jupyter notebook does not raise this error.
     def test_train_model(self):
 
         _bi_text = pd.read_csv("../../data/sentences_bilingual.csv")
@@ -117,45 +121,48 @@ class TestLangID(unittest.TestCase):
 
         assert_greater(_acc, 0.89)
 
-    def test_d2_3_train_model(self):
+    def test_train_model_multi(self):
+        multi_text = pd.read_csv("../../data/sentences_multilingual.csv")
+        multi_text_train, multi_text_test = train_test_split(multi_text, test_size=0.2)
 
-        _bi_text = pd.read_csv("../../data/sentences_bilingual.csv")
-        _c2i, _i2c = vocab.build_vocab(bi_text.sentence.values)
-        _l2i, _i2l = vocab.build_label_vocab(bi_text.lang.values)
+        multi_text.groupby('lang').count()
+        multi_text_train.groupby('lang').count()
 
-        _li = lang_id.LangID(
+        _c2i, _i2c = vocab.build_vocab(multi_text.sentence.values)
+        _l2i, _i2l = vocab.build_label_vocab(multi_text.lang.values)
+
+        multi_class = lang_id.LangID(
             input_vocab_n=len(_c2i),
             embedding_dims=10,
             hidden_dims=20,
             lstm_layers=1,
-            output_class_n=2
+            output_class_n=5
         )
 
-        trained_model = lang_id.train_model(
-            model=_li,
+        lang_id.train_model(
+            model=multi_class,
             n_epochs=1,
-            training_data=bi_text_train,
+            training_data=multi_text_train,
             c2i=_c2i, i2c=_i2c,
             l2i=_l2i, i2l=_i2l
-        )
+        );
+        print("done")
 
-        trained_model(vocab.sentence_to_tensor("this is a sentence", _c2i))
-        trained_model(vocab.sentence_to_tensor("quien estas", _c2i))
+        acc_multi, y_hat_multi = lang_id.eval_acc(multi_class, multi_text_test, _c2i, _i2c, _l2i, _i2l)
 
-        _acc, _y_hat = lang_id.eval_acc(trained_model, bi_text_test, _c2i, _i2c, _l2i, _i2l)
-        print(f"Trained Accuracy: {_acc}")
+        # Jupyter reported Accuracy: 0.6954
+        print(f"Accuracy: {acc_multi}")
+        assert_greater(acc_multi, 0.69)
 
-        _untrained = lang_id.LangID(
-            input_vocab_n=len(_c2i),
-            embedding_dims=10,
-            hidden_dims=20,
-            lstm_layers=1,
-            output_class_n=2
-        )
+        from sklearn.metrics import classification_report, confusion_matrix
+        y_multi = multi_text_test.lang.values
+        print(classification_report(y_multi, y_hat_multi))
 
-        _acc_untrained, _y_hat_untrained = lang_id.eval_acc(_untrained, bi_text_test, _c2i, _i2c, _l2i, _i2l)
-        print(f"Untrained Accuracy: {_acc_untrained}")
+        cm = confusion_matrix(y_multi, y_hat_multi)
+        cm
 
-        assert_greater(_acc, 0.89)
+        #reload(lang_id);
+        lang_id.pretty_conf_matrix(cm, ['deu', 'eng', 'fra', 'ita', 'spa'])
+
 
 
