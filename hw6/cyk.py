f10b215442b93642f27e2e1fed87a820fa8b25bb
steve3p0 <sbraich@bpdx.edu>
2019-09-05 11:35:10 -0700

    cyk parsing indexing is off

--- hw6/cyk.py
+++ hw6/cyk.py
@@ -26,7 +26,51 @@ class Cyk(object):
 
         return rules
 
-    def parse(self, s):
+    def parse2(self, s):
+
+        words = s.split(' ')
+        n = len(words)
+        table = pandas.DataFrame(index = range(n), columns = words)
+        table.fillna('', inplace=True)
+
+        print(table)
+
+        prev_left = ''
+
+        for i in range(n):
+
+            # initialize Uninary terminal cell
+            nt = [key[0] for key, value in self.pcfg.items() if words[i] in key[1]]
+            table.iloc[i, i] = nt[0]
+
+            # Find rule with previous terminal
+            nt_prev = table.iloc[i - 1, i - 1]
+            rhs = tuple([ nt_prev, nt[0] ])
+            lhs = [key[0] for key, value in self.pcfg.items() if rhs == key[1]]
+
+            if len(lhs) > 0:
+                table.iloc[i - 1, i] = lhs[0]
+                nt = lhs
+
+            # Find rule
+            # ј is the diagonal
+            for j in range(i, 0, -1):
+                for k in range(i, j, -1):
+                    if table.iloc[j-1, k] == '':
+                        table.iloc[j-1, k] = str(j)
+                        nt_prev = table.iloc[j-1, k]
+                        rhs = tuple([nt_prev, nt[0]])
+                        lhs = [key[0] for key, value in self.pcfg.items() if rhs == key[1]]
+
+                        if len(lhs) > 0:
+                            table.iloc[j, k] = lhs[0]
+                            nt = lhs
+
+                print(table)
+
+        print(table)
+
+    def parse0(self, s):
 
         words = s.split(' ')
         n = len(words)
@@ -68,55 +112,70 @@ class Cyk(object):
 
                 print(table)
 
-                # for j in range(i):
-                #
-                #     nt_prev = table.iloc[i - j, i - j]
-                #     rhs = [nt_prev, nt[0]]
-                #     rhs = [nt_prev, nt[0]]
-                #     rhs = tuple([nt_prev, nt[0]])
-                #
-                #     key1 = [key[1] for key, value in self.pcfg.items()]
-                #     lhs = [key[0] for key, value in self.pcfg.items() if rhs == key[1]]
-                #
-                #     if len(lhs) > 0:
-                #         table.iloc[j - 1, j] = lhs[0]
+        print(table)
 
+    def parse(self, s):
+
+        words = s.split(' ')
+        n = len(words)
+        table = pandas.DataFrame(index = range(n), columns = words)
+        table.fillna('', inplace=True)
+
+        print(table)
+
+        term1 = ''
+        term2 = ''
+
+        for i in range(n):
+
+            # initialize Uninary terminal cell
+            nt = [key[0] for key, value in self.pcfg.items() if words[i] in key[1]]
+            table.iloc[i, i] = nt[0]
+
+            # Find rule with previous terminal
+            nt_prev = table.iloc[i - 1, i - 1]
+            rhs = tuple([ nt_prev, nt[0] ])
+            lhs = [key[0] for key, value in self.pcfg.items() if rhs == key[1]]
+
+            if len(lhs) > 0:
+                table.iloc[i - 1, i] = lhs[0]
+                nt = lhs
+
+            # Find rule
+
+            # J IS ROW
+            for j in range(i, 0, -1):
+                # K IS COLUMN
+                for k in range(i, 0, -1):
+                    if table.iloc[j-1, k] == '':
+                        # LOOKING TO PUT LHS
+                        table.iloc[j-1, k] = str(j-1) + ', ' + str(k)
+
+                        # RHS: TERM 1
+                        nt_prev = table.iloc[j, j]
+                        term1 = nt_prev
+                        term2 = nt[0]
+
+                        # RHS: TERM 1 + TERM 2
+                        #rhs = tuple([nt_prev, nt[0]])
+                        rhs = tuple([term1, term2])
+
+                        # LHS: lookup using rhs
+                        lhs = [key[0] for key, value in self.pcfg.items() if rhs == key[1]]
+                        if len(lhs) > 0:
+                            table.iloc[j, k] = lhs[0]
+                            nt = lhs
+
+
+                    print("######################################")
+                    print(f"term 1: {term1}")
+                    print(f"term 2: {term2}")
+                    print(f"rhs: {rhs}")
+                    print(f"i: {i}")
+                    print(f"j: {j}")
+                    print(f"k: {k}")
+                    print(table)
 
         print(table)
 
         print("Hello!")
-
-            #nonterminal = [key[0] for key, value in self.pcfg.items() if words[i] in key[1]]
-            #lookup = {('the',): 1.0}
-            #list(self.pcfg.values()).index({('the',): 1.0})
-            #print(nonterminal)
-            #nonterminal = self.pcfg[words[i]]
-            #print(list(mydict.keys())[list(mydict.values()).index(16)])  #
-            #print(table.loc[i, "Name"], table.loc[i, "Age"])
-            #table.iloc[i,]
-
-        #raise NotImplementedError
-
-        #
-        # for j in range(n):
-
-        # let the input be a string I consisting of n characters: a1 ... an.
-        # let the grammar contain r nonterminal symbols R1 ... Rr, with start symbol R1.
-        # let P[n,n,r] be an array of real numbers. Initialize all elements of P to zero.
-        # let back[n,n,r] be an array of backpointing triples.
-        # for each s = 1 to n
-        #   for each unit production Rv →as
-        #     set P[1,s,v] = Pr(Rv →as)
-        # for each l = 2 to n -- Length of span
-        #   for each s = 1 to n-l+1 -- Start of span
-        #     for each p = 1 to l-1 -- Partition of span
-        #       for each production Ra → Rb Rc
-        #         prob_splitting = Pr(Ra →Rb Rc) * P[p,s,b] * P[l-p,s+p,c]
-        #         if P[p,s,b] > 0 and P[l-p,s+p,c] > 0 and P[l,s,a] <  prob_splitting then
-        #           set P[l,s,a] = prob_splitting
-        #           set back[l,s,a] = <p,b,c>
-
-
-
-
-
