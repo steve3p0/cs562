1f4a9a372f60827023ba5dd1097bff0c46ef12c5
steve3p0 <sbraich@bpdx.edu>
2019-09-07 02:59:07 -0700

    Getting a reworked algorithm... working.

--- hw6/test_cyk.py
+++ hw6/test_cyk.py
@@ -177,6 +177,25 @@ class TestCyk(unittest.TestCase):
 
     def test_python_ranges(self):
 
+        cyk = Cyk()
+
+        rules = [
+            ('S', ['NP', 'VP']),
+            ('NP', ['DT', 'NN']),
+            ('VP', ['VB', 'NP']),
+            ('DT', ['the']),
+            ('NN', ['teacher']),
+            ('NN', ['lecture']),
+            ('VB', ['gave']),
+        ]
+
+        pcfg = cyk.tree.convert_to_pcfg(rules)
+        cyk.rules = rules
+        cyk.pcfg = pcfg
+        pcfg_pretty = cyk.tree.pretty_pcgf(pcfg)
+        #actual = format(pcfg)
+        print(pcfg_pretty)
+
         s = "the teacher gave the lecture"
         words = s.split(' ')
         n = len(words)
@@ -187,14 +206,275 @@ class TestCyk(unittest.TestCase):
         print(table)
 
         step = 0
+        nt_loc = ''
+        nt_prev = ''
+        nt_prev_loc = ''
+        lhs_term = ''
+        lhs_loc = ''
+        red_term_left = ''
+        # for x in range(n):
+        #for y in range(x, -1, -1):
+
+        lhs_x = 0
+        lhs_y = 0
+        rhs_left_x = 0
+        rhs_left_y = 0
+        rhs_right_x = 0
+        rhs_right_y = 0
+
+        step = 0
+        for y in range(n):
+            for x in range(y, -1, -1):
+                step += 1
+                red_term_right = format(f"R-R:{str(step)}")
+                table.iloc[x, y] = red_term_right
+                rhs2_ptr = table.iloc[x, y]
+                rhs_right_x = x
+                rhs_right_y = y
+
+                if x > 0:
+                    step += 1
+                    green_lhs = format(f"G:{str(step)}")
+                    table.iloc[x-1, y] = green_lhs
+                    lhs_ptr = table.iloc[x-1, y]
+                    lhs_x = x - 1
+                    lhs_y = y
+
+                red_term_left = format(f"R-L:{str(step)}")
+                table.iloc[x, y] = red_term_left
+                rh1_ptr = table.iloc[x, y]
+                rhs_left_x = x
+                rhs_left_y = y
+
+                print(table)
+
+                for z in range (y, x, -1):
+                    #if x > 0:
+                    step += 1
+                    green_lhs = format(f"G:{str(step)}")
+                    table.iloc[x, z] = green_lhs
+                    lhs_ptr = table.iloc[x, z]
+                    lhs_x = x
+                    lhs_y = z
+
+                    print(table)
+
+
+
+
+        # for y in range(n):
+        #     for z in range(y, -1, -1):
+        #         step += 1
+        #         #table.iloc[y, z] = str(step)
+        #
+        #         nt = [key[0] for key, value in pcfg.items() if words[y] in key[1]]
+        #         nt = nt[0]
+        #
+        #         rhs = tuple([nt_prev, nt])
+        #         lhs = [key[0] for key, value in pcfg.items() if rhs == key[1]]
+        #
+        #         # Set terminal cell
+        #         if table.iloc[y, y] == '':
+        #             table.iloc[y, y] = nt
+        #             nt_loc = format(f"{y}, {y}")
+        #
+        #         # Set LHS of Rule
+        #         if len(lhs) > 0:
+        #             lhs_term = lhs[0]
+        #             table.iloc[y, y - 1] = lhs[0]
+        #             lhs_loc = format(f"{y}, {y - 1}")
+        #
+        #         print("######################################")
+        #         print(f"    lhs[{lhs_loc}]: {lhs_term}")
+        #         print(f"nt_prev[{nt_prev_loc}]: {nt_prev}")
+        #         print(f"     nt[{nt_loc}]: {nt}")
+        #         print(f"rhs: {rhs}")
+        #         # print(f"i: {i}")
+        #         # print(f"j: {j}")
+        #         # print(f"k: {k}")
+        #         print(f"y: {y}")
+        #         print(f"z: {z}")
+        #         print(table)
+        #
+        #         if len(lhs) > 0:
+        #             nt_loc = format(f"{y}, {y - 1}")
+        #             nt = lhs[0]
+        #         # else:
+        #
+        #         # Find nt_previous
+        #         nt_prev = table.iloc[y - 1, z]
+        #         nt_prev_loc = format(f"{y - 1}, {z}")
+        #         # nt_prev = table.iloc[y, z - 1]
+        #         # nt_prev_loc = format(f"{y}, {z - 1}")
+
+
+        # step = 0
+        # for x in range(n):
+        #     for y in range(x, -1, -1):
+        #         step += 1
+        #         #table.iloc[x - 1, y] = str(step) + ": " + str(x - 1) + ', ' + str(y)
+        #         table.iloc[x, y] = str(step)
+        #         #print(f"x,y: {x},{y}")
+        #
+        #         print(table)
+
+    def test_python_ranges1(self):
+
+        cyk = Cyk()
+
+        rules = [
+            ('S', ['NP', 'VP']),
+            ('NP', ['DT', 'NN']),
+            ('VP', ['VB', 'NP']),
+            ('DT', ['the']),
+            ('NN', ['teacher']),
+            ('NN', ['lecture']),
+            ('VB', ['gave']),
+        ]
+
+        pcfg = cyk.tree.convert_to_pcfg(rules)
+        cyk.rules = rules
+        cyk.pcfg = pcfg
+        pcfg_pretty = cyk.tree.pretty_pcgf(pcfg)
+        # actual = format(pcfg)
+        print(pcfg_pretty)
+
+        s = "the teacher gave the lecture"
+        words = s.split(' ')
+        n = len(words)
+
+        table = pandas.DataFrame(index=range(n), columns=words)
+        table.fillna('', inplace=True)
+
+        print(table)
+
+        step = 0
         for x in range(n):
             for y in range(x, -1, -1):
                 step += 1
-                #table.iloc[x - 1, y] = str(step) + ": " + str(x - 1) + ', ' + str(y)
-                table.iloc[x, y] = str(step)
-                #print(f"x,y: {x},{y}")
+
+                nt = [key[0] for key, value in pcfg.items() if words[x] in key[1]]
+                nt = nt[0]
+                table.iloc[x, x] = nt
+                nt_prev = table.iloc[x - 1, y]
+                rhs = tuple([nt_prev, nt])
+                lhs = [key[0] for key, value in pcfg.items() if rhs == key[1]]
+
+                if len(lhs) > 0:
+                    table.iloc[x, x - 1] = lhs[0]
+                    nt = lhs[0]
+
+                # else:
+                #     table.iloc[x, x - 1] = str(step)
+
+                # # Find rule with previous terminal
+                # #nt_prev = self.strip_formating(table.iloc[x - 1, x - 1])
+                # nt_prev = table.iloc[x + 1, + 1]
+                # rhs = tuple([nt_prev, nt[0]])
+                # lhs = [key[0] for key, value in pcfg.items() if rhs == key[1]]
+                #
+                # if len(lhs) > 0:
+                #     table.iloc[x - 1, x] = str(step) + ": " + lhs[0]
+                #     nt = lhs
+
+                # table.iloc[x - 1, y] = str(step) + ": " + str(x - 1) + ', ' + str(y)
+                # table.iloc[x, y] = str(step)
+                # print(f"x,y: {x},{y}")
 
                 print(table)
+
+        # step = 0
+        # for x in range(n):
+        #     for y in range(x, -1, -1):
+        #         step += 1
+        #         #table.iloc[x - 1, y] = str(step) + ": " + str(x - 1) + ', ' + str(y)
+        #         table.iloc[x, y] = str(step)
+        #         #print(f"x,y: {x},{y}")
+        #
+        #         print(table)
+
+    def test_python_ranges2(self):
+
+        cyk = Cyk()
+
+        rules = [
+            ('S', ['NP', 'VP']),
+            ('NP', ['DT', 'NN']),
+            ('VP', ['VB', 'NP']),
+            ('DT', ['the']),
+            ('NN', ['teacher']),
+            ('NN', ['lecture']),
+            ('VB', ['gave']),
+        ]
+
+        pcfg = cyk.tree.convert_to_pcfg(rules)
+        cyk.rules = rules
+        cyk.pcfg = pcfg
+        pcfg_pretty = cyk.tree.pretty_pcgf(pcfg)
+        #actual = format(pcfg)
+        print(pcfg_pretty)
+
+        s = "the teacher gave the lecture"
+        words = s.split(' ')
+        n = len(words)
+
+        table = pandas.DataFrame(index = range(n), columns = words)
+        table.fillna('', inplace=True)
+
+        print(table)
+
+        step = 0
+        nt_loc = ''
+        nt_prev = ''
+        nt_prev_loc = ''
+        lhs_term = ''
+        lhs_loc = ''
+        # for x in range(n):
+        #for y in range(x, -1, -1):
+        for y in range(n):
+            for z in range(y, -1, -1):
+                step += 1
+                #table.iloc[y, z] = str(step)
+
+                nt = [key[0] for key, value in pcfg.items() if words[y] in key[1]]
+                nt = nt[0]
+
+                rhs = tuple([nt_prev, nt])
+                lhs = [key[0] for key, value in pcfg.items() if rhs == key[1]]
+
+                # Set terminal cell
+                if table.iloc[y, y] == '':
+                    table.iloc[y, y] = nt
+                    nt_loc = format(f"{y}, {y}")
+
+                # Set LHS of Rule
+                if len(lhs) > 0:
+                    lhs_term = lhs[0]
+                    table.iloc[y, y - 1] = lhs[0]
+                    lhs_loc = format(f"{y}, {y - 1}")
+
+                print("######################################")
+                print(f"    lhs[{lhs_loc}]: {lhs_term}")
+                print(f"nt_prev[{nt_prev_loc}]: {nt_prev}")
+                print(f"     nt[{nt_loc}]: {nt}")
+                print(f"rhs: {rhs}")
+                # print(f"i: {i}")
+                # print(f"j: {j}")
+                # print(f"k: {k}")
+                print(f"y: {y}")
+                print(f"z: {z}")
+                print(table)
+
+                if len(lhs) > 0:
+                    nt_loc = format(f"{y}, {y - 1}")
+                    nt = lhs[0]
+                # else:
+
+                # Find nt_previous
+                nt_prev = table.iloc[y - 1, z]
+                nt_prev_loc = format(f"{y - 1}, {z}")
+                # nt_prev = table.iloc[y, z - 1]
+                # nt_prev_loc = format(f"{y}, {z - 1}")
 
 
         # step = 0
